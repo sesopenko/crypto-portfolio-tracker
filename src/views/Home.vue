93151755b6a207049adf7d50b257623408ed3ce2
Sean Esopenko <sesopenko@gmail.com>
2018-02-24 14:51:45 -0800

    Display holding list & holding creator

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="home">
-    This is the home
+    <HoldingCreator/>
+    <HoldingList/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import HoldingCreator from '@/components/HoldingCreator';
+import HoldingList from '@/components/HoldingList';
 
 export default Vue.extend({
   name: 'Home',
+  components: {
+    HoldingCreator,
+    HoldingList,
+  },
 });
 </script>
